abe3f599b732bac02809c9de4bc7a91c25121bf4
Oleksandr Nikolaiev <remixod@gmail.com>
2022-07-22 18:57:13 +0200

    update

--- dist/plugin-registration/PluginRegistration.ps1
+++ dist/plugin-registration/PluginRegistration.ps1
@@ -17,16 +17,16 @@ $ErrorActionPreference = "Stop"
 Write-Verbose 'Entering PluginRegistration.ps1'
 
 #Print Verbose
-Write-Host "crmConnectionString = $CrmConnectionString"
-Write-Host "registrationType = $RegistrationType"
-Write-Host "assemblyPath = $AssemblyPath"
-Write-Host "projectFilePath = $ProjectFilePath"
-Write-Host "useSplitAssembly = $UseSplitAssembly"
-Write-Host "MappingFile = $MappingFile"
-Write-Host "solutionName = $SolutionName"
-Write-Host "crmConnectionTimeout = $CrmConnectionTimeout"
+Write-Host "crmConnectionString = " $CrmConnectionString
+Write-Host "registrationType = " $RegistrationType
+Write-Host "assemblyPath = " $AssemblyPath
+Write-Host "projectFilePath = " $ProjectFilePath
+Write-Host "useSplitAssembly = " $UseSplitAssembly
+Write-Host "MappingFile = " $MappingFile
+Write-Host "solutionName = " $SolutionName
+Write-Host "crmConnectionTimeout = " $CrmConnectionTimeout
 
 
-& "$PSScriptRoot\..\ps_modules\lib\xRMCIFramework\9.0.0\PluginRegistration.ps1" -CrmConnectionString "$CrmConnectionString" -RegistrationType "$RegistrationType" -AssemblyPath "$AssemblyPath" -MappingFile "$MappingFile" -SolutionName "$SolutionName" -useSplitAssembly "$UseSplitAssembly" -projectFilePath "$ProjectFilePath" -Timeout $CrmConnectionTimeout
+& "$PSScriptRoot\..\ps_modules\lib\xRMCIFramework\9.0.0\PluginRegistration.ps1" -CrmConnectionString "$CrmConnectionString" -RegistrationType "$RegistrationType" -AssemblyPath "$AssemblyPath" -MappingFile "$MappingFile" -SolutionName "$SolutionName" -useSplitAssembly $UseSplitAssembly -projectFilePath "$ProjectFilePath" -Timeout $CrmConnectionTimeout
 
 Write-Verbose 'Leaving MSCRMPluginRegistration.ps1'
